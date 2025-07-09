61254fa9ffb77ebb3ed9d45e5032d1a07cd975a1
michaelmyers23 <45492857+michaelmyers23@users.noreply.github.com>
2025-07-09 16:31:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,23 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-function InserirImagem(url) {
-        let img = document.createElement("img");
-        img.src=url;
-        document.body.appendChild(img);
-    }
-
 const codigos = {
-  "ABC123": "Tudo certo",
-  "DEF456": "Ótimo! Aqui está a Mensagem 2.\n Quebrou a linha InserirImagem(https://gru.ifsp.edu.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png)",
-  "XYZ789": "Sucesso! Mensagem secreta 3 revelada.",
-  "Azerbaijão": "Ainda assim funciona.\n Quebrou a linha",
+  "ABC123": { texto: "Tudo certo" },
+  "DEF456": { 
+    texto: "Ótimo! Aqui está a Mensagem 2.\nQuebrou a linha", 
+    imagem: "https://gru.ifsp.edu.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" 
+  },
+  "XYZ789": { texto: "Sucesso! Mensagem secreta 3 revelada." },
+  "Azerbaijão": { texto: "Ainda assim funciona.\nQuebrou a linha" },
 };
 
 app.post('/verificar', (req, res) => {
   const { codigo } = req.body;
 
   if (codigos[codigo]) {
-    res.json({ sucesso: true, mensagem: codigos[codigo] });
+    res.json({ sucesso: true, ...codigos[codigo] });
   } else {
-    res.json({ sucesso: false, mensagem: "Código inválido." });
+    res.json({ sucesso: false, texto: "Código inválido." });
   }
 });
 
