c164692ae539bc8570970830a6c9eb30b1a554f6
michaelmyers23 <45492857+michaelmyers23@users.noreply.github.com>
2025-07-10 20:02:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -187,18 +187,46 @@ Quae vicinitas haec tabula vocare vis?`
     "Quadra de futebol society":  { mensagem: "Entre risos que ecoam como sinos de vento, <br>E onde o chão guarda marcas de aventuras em movimento, <br>Há um reino sem tronos, mas com escaladas, <br>E soldados de areia em guerras encantadas. <br>Os adultos passam, mas poucos entendem, <br>Que ali o tempo para e os sonhos se estendem. <br>Se procuras o próximo passo com alegria e cor, <br>Siga onde balanços embalam o riso e o amor." },
   },
   VerdeEscuro: {
-    "ABC123": { mensagem: "1" },
-    "DEF456": {
-      mensagem: `2<img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" alt="Imagem" style="max-width:100%;">`
-    },
-    "XYZ789": { mensagem: "3" },
-    "pipipi": { mensagem: "4" },
-    "papapa": { mensagem: "5" },
-    "popopo": { mensagem: "6" },
-    "pupupu": { mensagem: "7" },
-    "pepepe": { mensagem: "8" },
-    "cracra": { mensagem: "9" },
-    "sabão": { mensagem: "10" }
+    "Verde Escuro": { mensagem: `
+ܠܐ ܐܢܐ ܐܢܫܐ، ܐܠܐ ܐܒܥܝ ܚܘܒܐ، <br>
+ܐܚܙܐ ܒܟܬܒܐ ܚܘܒܐ ܕܝܢܐ ܐܬܪܝܐ. <br>
+ܐܢܐ ܩܛܥܐ ܕܩܘܦܬܐ، ܨܘܪܬܐ ܘܪܘܚܢܘܬܐ، <br>
+ܡܐ ܫܡܝ، ܣܝܡܢܐ ܕܡܕܝܢܬܐ ܘܠܒܐ؟` },
+    "Eu amo Ponta Grossa": {
+      mensagem: `ၵဝ်ဢမ်ႇၵိူတ်ႇမႃးတီႈပၢင်ႇလၢႆႇသေတႃႉ ၵဝ်ယိပ်းၵမ်ဝႆႉၶူဝ်းၶွင် ဢၼ်မီးၼႂ်းၸိုဝ်ႈၵဝ်။ <br>
+ၵဝ် ဢမ်ႇၸႂ်ႈ ၵႅဝ်ႈ ဢမ်ႇၸႂ်ႈ ၵွၼ်ႈ ၵူၺ်းၵႃႈ ပဵၼ် လႅင်း ဢၼ် ဢိုပ်းယၢၵ်ႈ ၵိၼ် ၼၼ်ႉ ဢေႃႈ။ <br>
+ၼႂ်းၵႄႈပၢင်ႇလၢႆႇၶူဝ်လမ် လႄႈ လူမ်းထၢၼ်ႇႁိၼ်၊ <br>
+ၵဝ်သိမ်းဝႆႉလွင်ႈလပ်ႉလွင်း ၼႂ်းၶဝ်ႈမုၼ်း ဢၼ်တဵမ်ဝႆႉ တင်းၸႂ်ႁၵ်ႉ။ <br>
+သီၵဝ် ႁဵတ်းႁႂ်ႈမီးလွင်ႈလပ်ႉလွင်း ဢၢႆႁွမ်ၵဝ် လႅၼ်ႈႁိူဝ်းယူႇ၊ <br>
+ၸိူဝ်းႁၼ်ၵဝ်ၼၼ်ႉ ဢမ်ႇလိုမ်း- ၶဝ်တိုၼ်းပွၵ်ႈမိူဝ်း ဢႅၼ်ႇၵႃႇ။ <br>
+ၵဝ်ပဵၼ်ၽႂ် ဢၼ်လႅၼ်ႈၼိူဝ်ၽႅၼ်ႇသေ ဢမ်ႇလႅၼ်ႈသေ ဢွင်ႇပေႉ။`},
+    "Hamburgueria Pérola Negra": { mensagem: `In hac tabula quae hic stat ad loquendum,<br>
+Arcanas fabulas invenies legendum.<br>
+Vicinitas est quae lutum sibi propositum fecit,<br>
+Terram in artem et passionem vertit.<br>
+Manus quae formant tempore ac cura,<br>
+Creant ea quae ignis signat dura.<br>
+Vici servantes traditionem et dignitatem,<br>
+Ubi praeteritum cum amore miscetur.<br>
+Si omnia haec legendo scire vis,<br>
+Quae vicinitas haec tabula vocare vis?` },
+    "História de Olarias": { mensagem: `Χωρίς στέγη με σκεπάζω, μα τ’ άστρα με κοιτούν,<br>
+Η φωνή μου αντηχεί όπου οι άνεμοι περνούν.<br>
+Είμαι πέτρα, είμαι χορτάρι, είμαι ήχος που απλώνεται,<br>
+Στη μέση του πουθενά, η τέχνη δεν σωπαίνει.<br>
+Χωρίς κουρτίνες ή τοίχους να με περιορίζουν,<br>
+Είμαι σκηνή της ζωής, του γέλιου και της γνώσης.<br>
+Ποιος είμαι εγώ, που σχηματίστηκα στη γη και στον χώρο,<br>
+Όπου ο ουρανός είναι η στέγη και ο χρόνος το μέτρο;
+` },
+    "Anfiteatro": { mensagem: "我浩瀚無垠，卻非大海；我金碧輝煌，卻不閃耀。 <br>在我心中，沒有想要停留的足跡。 <br>我接受跳躍、發球、跌倒和榮耀， <br>但我沒有勝利，只有回憶。 <br>沒有牆壁，沒有天花板，任憑風吹拂， <br>我是誰 在遊戲與生活相遇的地方？" },
+    "Quadra de areia": { mensagem: "ຈາກກ້າງສູງຂ້າມເບິ່ງໂລກໂດຍບໍ່ຂຍື້ນຕົວ, <br> ເປີດເຫັນຂອບເຂດທີ່ບາງຄົນເຫັນບໍ່ໄດ້.  <br> ຂ້ອຍແມ່ນຈຸດຕິດຕາມສະຫນາມ, ຜູ້ຮັບຜິດຊອບການເບິ່ງເຫັນ, <br> ລະຫວ່າງຟ້າແລະດິນ, ຂ້ອຍເຊີນໃຫ້ຝັນ.  <br> ບໍ່ມີປີກ, ຂ້ອຍບິນ; ບໍ່ມີຂາ, ຂ້ອຍເດີນ, <br> ໃຜຄືຂ້ອຍ ຜູ້ທີ່ສະແດງໃຫ້ເຫັນຄວາມບໍ່ມີຂອບເຂດ ຢ່າງໂດດຫວ່າງ?" },
+    "Mirante": { mensagem: "Ni seulement jus, ni seulement cocktail en main,<br>Ici se mêlent fête, rires et refrains.<br>Glace, saveurs, couleurs qui dansent,<br>Qui suis-je, celui qui t’invite à faire une danse?<br>Je suis le lieu où tout se mélange,<br>Entre amis, la joie est un mélange étrange.<br>Quel est mon nom, où la nuit est mixée,<br>Et où l’amusement ne cesse jamais ?" },
+    "Tudo e Misturado": { mensagem: `སྒྲ་མེད་གདོང་སྐྱོང་མ། ང་ནགས་ལྡོག་དང་མྱུར་དྲག་གཉིས་དུ་མཐུན་ལམ་བཞུགས། <br>
+ང་ནང་ལ་ཚོན་མོ་མ་འཕྲོས་པའི་ཚེ་སྲོག་ཞིང་ཞུ། <br> 
+ང་ནི་རོགས་ལེན་དང་བཀོད་སྐྱོང་ཡིན། ང་ནི་མི་རྣམ་མཁྱེན་གྱི་རྩ་རྒྱུན་ཡིན། <br>
+ང་སུ་ཞིག་ནམ་མཁའི་དུས་ལ་རྟོགས་མཛད་པ་རྒྱུ་ཡིན་ན།` },
+    "Mão": { mensagem: "Entre risos que ecoam como sinos de vento, <br>E onde o chão guarda marcas de aventuras em movimento, <br>Há um reino sem tronos, mas com escaladas, <br>E soldados de areia em guerras encantadas. <br>Os adultos passam, mas poucos entendem, <br>Que ali o tempo para e os sonhos se estendem. <br>Se procuras o próximo passo com alegria e cor, <br>Siga onde balanços embalam o riso e o amor." },
   },
   Branco: {
     "ABC123": { mensagem: "1" },
